4017afc0204a465037856985313453141704f715
Silvia Brown <siltastic@gmail.com>
2015-07-12 11:07:03 -0400

    Added basic js file.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Necessary Deliverables
+    // A hosted copy of your game or app, built by you, hosted on Github pages.
+    // Your HTML/CSS should pass the HTML5 Validator and CSS Validators
+    // A git repository hosted on Github with frequent commits.
+    // A link to your hosted project in the URL section of your Github repo
+    // A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+
+// Technical Requirements
+    // Render in the browser.
+    // Include separate HTML, CSS, and JavaScript files.
+    // Use with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles.
+    // Use JavaScript or jQuery for DOM manipulation.
+    // Deploy online, using GitHub project pages.
+    // Use semantic markup for HTML and CSS.
+
+// Trivia Game
+// A simple trivia game with "subject" related questions.
+
+// Bonus:
+    // Add time-based scoring.
+    // Track scores across games (even if the page is reloaded).
+    // Allow users to compete against each other on a high-score board.
+
+// User Stories
+// As a user, I should be able to ...
+    // Answer a question in order to continue with the trivia.
+    // Know if my answer was correct in order to feel accomplished.
+    // Know if my answer was incorrect in order to learn the correct answer.
+    // Know what my score is in order to keep track of my correct answers.
+    // Know which question I am currently on in order to keep track of my progress.
+    // See a list of questions with the correct answers to learn where I was correct or incorrect.
+    // Select a trivia topic in order to enjoy the trivia game.
+    // Select a level in order to test my intelligence.
+    // Skip a question in order to avoid a question.
+
+
+// Psuedocode
