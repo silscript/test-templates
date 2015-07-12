20039173acf2759fdf32b096569d786a8f4cf380
Silvia Brown <siltastic@gmail.com>
2015-07-12 12:48:47 -0400

    Added pseudocode and edited instructions.

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
     // Use semantic markup for HTML and CSS.
 
 // Trivia Game
-// A simple trivia game with "subject" related questions.
+    // A simple trivia game with "subject" related questions.
 
 // Bonus:
     // Add time-based scoring.
@@ -22,16 +22,50 @@
     // Allow users to compete against each other on a high-score board.
 
 // User Stories
-// As a user, I should be able to ...
-    // Answer a question in order to continue with the trivia.
-    // Know if my answer was correct in order to feel accomplished.
-    // Know if my answer was incorrect in order to learn the correct answer.
-    // Know what my score is in order to keep track of my correct answers.
-    // Know which question I am currently on in order to keep track of my progress.
-    // See a list of questions with the correct answers to learn where I was correct or incorrect.
-    // Select a trivia topic in order to enjoy the trivia game.
-    // Select a level in order to test my intelligence.
-    // Skip a question in order to avoid a question.
-
+    // As a user, I should be able to ...
+        // Answer a question in order to continue with the trivia.
+        // Know if my answer was correct in order to feel accomplished.
+        // Know if my answer was incorrect in order to learn the correct answer.
+        // Know what my score is in order to keep track of my correct answers.
+        // Know which question I am currently on in order to keep track of my progress.
+        // See a list of questions with the correct answers to learn where I was correct or incorrect.
+        // Select a trivia topic in order to enjoy the trivia game.
+        // Select a level in order to test my intelligence.
+        // Skip a question in order to avoid a question.
 
 // Psuedocode
+    // User load website on their computer.
+    // Website loads all the necessary files and functions.
+    // User views website and reads instructions.
+    // User decided which level of difficulty.
+        // Beginner
+        // Intermediate
+        // Advanced
+    // If user clicks on one of the following buttons.
+        // Beginner
+            // Beginner level questions will load.
+        // Intermediate
+            // Intermediate level questions will load.
+        // Advaned
+            // Advanced level questions will load.
+    // All questions will append the css style when they load.
+    // User sees the first question.
+    // User will decide which option to answer.
+        // If user is correct ...
+            // Correct answer will turn green.
+            // One point will be added to the score (#/##).
+        // If user is incorrect ...
+            // Incorect answer will turn red.
+            // Correct answer will turn green.
+            // No points will be added to the score.
+    // User will click the next question button.
+    // Next question will load.
+    // User will repeat the previous steps until all questions are answered.
+    // When all questions are answer ...
+        // Score will appear.
+        // Will you like play again?
+            // Yes or no?
+                // If yes ...
+                    // User will choose level of difficulty again.
+                // If no ...
+                    // Website will load to the instructions.
