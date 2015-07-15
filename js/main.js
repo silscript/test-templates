b51361a1348bd72204eae722f0a8a9ea8cbea734
Silvia Brown <siltastic@gmail.com>
2015-07-15 13:01:33 -0400

    Added new questions and answers.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 $(document).ready(function() {
   console.log("Webpage is ready.");
 
-  // Create all the easy questions and ansers in an array and.
+
+  // Create all the easy questions and answers in an array and.
   var triviaQuizNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   var triviaQuizEasy = [{ 
                           question: "The painter Piet Mondrian belonged to the artistic school knowns as ...",
@@ -27,23 +28,23 @@ $(document).ready(function() {
                         },
 
                         {
-                          question: "The Italian painter who created angular modernist portraits with solid, dark eyes was ...",
-                          choices: ["Amadeo Modigliani", "Filippo Marinetti", "Pablo Picasso", "Salvador Dali"],
+                          question: "All the following artist were Abstract Expressionists except ...",
+                          choices: ["Andy Warhol", "Jackson Pollack", "Wassily Kandinsky", "Willem de Kooning"],
                         },
 
                         {
-                          question: "Who was the non-native Spanish painter whose singular work featured angular figures and thick outlines?",
-                          choices: ["Diego Valáquez", "El Greco", "Hans Holbein", "Titian"],
+                          question: "The Impressionist painter who was particularly known for his still lifes was ...",
+                          choices: ["Claude Monet", "Édourad Manet", "Paul Cézanne", "Pierre-August Renoir"],
                         },
 
                         {
-                          question: "Surrealism is the style most closely associated with which of the following artists?",
-                          choices: ["Amadeo Modigliani", "Henri Matisse", "Pablo Picasso", "Salvador Dali"],
+                          question: "The Florentine noble family whose members were patrons of artists like Michelangelo, Leonardo da Vinci, and Botticelli were the ...",
+                          choices: ["Borgias", "Habsburgs", "Machiavellis", "Medicis"],
                         },
 
                         {
-                          question: "The artist who created the famous painting of Holy Roman Emperor Charles V on horseback was ...",
-                          choices: ["Diego Valáquez", "Hans Holbein", "Sandro Botticelli", "Titian"],
+                          question: "Screen printing is an artistic method that allows for ...",
+                          choices: ["transforms the colors on a canvas paiting.", "transforms the brushstrokes in a painting into different shapes.", "reproduction of an image on different pieces of art.", "projects an image onto a canvas to allow for hand copying."],
                         },
 
                         {
@@ -56,13 +57,16 @@ $(document).ready(function() {
                           choices: ["history paintings", "landscapes", "nudes", "portraits"],
                         }];
 
+
   // Create all the normal questions and answers in an object of the array.
   // var triviaQuizNorm = {}
   // console.log(triviaQuizNorm)
 
+
   // Create all the hard questions and answers in an object of the array.
   // var triviaQuizHard = {}
   // console.log(triviaQuizHard)
+
 
   // Create the trivia question structure.
   for (var i = 0; i < triviaQuizEasy.length; i++) {
@@ -71,53 +75,78 @@ $(document).ready(function() {
     // Append the individual ".trivia-question" in the ".site-container".
     // Append the divs in the ".site-container".
     // Add question number to each <div>.
-    $(".site-container").append("<div class='trivia-container animation'>" + "<div class='content-left'><h2 class='trivia-number'>" + triviaQuizNumbers[i] + "</h2></div>" + "<div class='content-right'><h2 class='trivia-question'>" + triviaQuizEasy[i]["question"] + "</h2></div></div>");
+    $(".site-container").append("<div class='trivia-container animation'>" + 
+                                "<div class='content-left'><h2 class='trivia-number'>" + triviaQuizNumbers[i] + "</h2></div>" + 
+                                "<div class='content-right'><h2 class='trivia-question'>" + triviaQuizEasy[i]["question"] + "</h2></div></div>");
       console.log("Created base.");
+
 
     // Create individual <ul> and append them to each object.
     $(".content-right").eq(i).append("<ul class='trivia-choices'>" + "</ul>");
       console.log("Created list.");
 
+
     // Locate the "choices" key in the object of the array.
     // Use "j" instead of "i" because "i" is being used for each element group of the outer array.
     for (var j = 0; j < triviaQuizEasy[i]["choices"].length; j++) {
+
 
       // Create individual <li> to each object's choices key list.
       $(".trivia-choices").eq(i).append("<li class='choice'>" + triviaQuizEasy[i]["choices"][j] + "</li>");
         console.log("Created choices.")
 
+
     }; // Closes "triviaQuizEasy.length["choices"]
   }; // Closes "triviaQuizEasy.length
+
 
   // Add animation class to each "trivia-container".
   $(".trivia-container").eq(0).addClass("fade-l");
   $(".trivia-container").eq(1).addClass("fade-r");
   $(".trivia-container").eq(2).addClass("fade-l");
-  $(".trivia-container").eq(3).addClass("fade-r");  
+  $(".trivia-container").eq(3).addClass("fade-r");
   $(".trivia-container").eq(4).addClass("fade-l");
   $(".trivia-container").eq(5).addClass("fade-r");
   $(".trivia-container").eq(6).addClass("fade-l");
-  $(".trivia-container").eq(7).addClass("fade-r");  
+  $(".trivia-container").eq(7).addClass("fade-r");
   $(".trivia-container").eq(8).addClass("fade-l");
   $(".trivia-container").eq(9).addClass("fade-r");
+
 
   // Append the score value.
   $(".nav-list").append("<li id='trivia-score'></li>");
     console.log("Appended score.");
 
+
   // Create an array of all the correct answers.
-  var correctAnswers = ["Amadeo Modigliani", "De Stijl", "El Greco", "Jacque-Louis David", "Pop Art", "portraits", "Salvador Dali", "The Napoleonic Wars", "Titian", "trompe l'oeil",];
   var updateScore = 1;
+  var correctAnswer = [ "Andy Warhol",
+                        "De Stijl", 
+                        "El Greco", 
+                        "Jacque-Louis David", 
+                        "Medicis",
+                        "Paul Cézanne",
+                        "Pop Art", 
+                        "portraits", 
+                        "reproduction of an image on different pieces of art.",
+                        "The Napoleonic Wars", 
+                        "trompe l'oeil"
+                      ];
+
 
   // Add an event listener to all <li> tags.
   $(".choice").on("click", function() {
     console.log("this =", this);
 
-    var questionContainer = $(this).closest(".trivia-container")
+    // Create the closest or current "trivia-container".
+    var questionContainer = $(this).closest(".trivia-container");
 
+    // Find "trivia-container" that has the "answered" class.
     if (questionContainer.hasClass("answered")) {
-      return // to stop it
+      // If found, stop searching.
+      return
     };
+
     questionContainer.addClass("answered");
 
     // Match answers to the individual <li> tags.
@@ -125,17 +154,18 @@ $(document).ready(function() {
     console.log(userChoice);
 
     // Loop through all the answers in the "correctAnswers" array.
-    for (var i = 0; i < correctAnswers.length; i++) {
+    for (var i = 0; i < correctAnswer.length; i++) {
 
       // If the answer is correct ...
-      if (correctAnswers[i] === userChoice) {
-        $(this).closest(".choice").addClass("correct")
-        console.log("Answer is correct!");
-        // Append the "correct" class. use the questioncontainer
+      if (correctAnswer[i] === userChoice) {
 
-         // Add 1 point to each correct answer.
+        // Add "correct" styling if correct.
+        $(this).closest(".choice").addClass("correct correct::after");
+          console.log("Answer is correct!");
+
+        // Add 1 point to each correct answer.
         $("#trivia-score").html(updateScore);
-          updateScore = updateScore + 1;
+          updateScore = updateScore + 1;  
           console.log("Added 1pt.");
       }; // Closes "if" statement
     }; // Closes "correctAnswers"
